96d50bf97c4ba33c57336b4b1095ca7d88a071b0
Anja_Kaufmann <anja@Anjas-MBP.fritz.box>
2015-05-14 16:20:57 +0200

    commits

--- Projekt/server.js
+++ Projekt/server.js
@@ -1,11 +1,30 @@
 var express = require('express');
-//var bodyParser = require ('body-parser');
-//var jsonParser = bodyParser.json();
+var bodyParser = require ('body-parser');
+var jsonParser = bodyParser.json();
 
 var app = express();
 
+//Daten erstellen
+var data = [
+  {title: "Hello World"},
+  {title: "Test Titel 2"}
+]
+
+//Response senden "Hello World"
 app.get('/', function (req, res){
   res.send('Hello World');
 });
+
+//Statuscode 200 zurückgeben - alles ok
+app.get('/data', function(req,res){
+  res.status(200).json(data);
+});
+
+//Errorhandler
+app.use(function(err,req,res,next){
+  console.error(err.stack);
+  res.end(err.status + '' + err.messages);
+});
+
 
 app.listen(3000);
