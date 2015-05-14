04f6c8b9c458409d5b141294931887c8e16452e3
alexander-mueller <mueller.alexanderjan@gmail.com>
2015-05-14 17:40:15 +0200

    User datei

--- Projekt/server.js
+++ Projekt/server.js
@@ -1,15 +1,15 @@
 var express     = require('express');
-var bodyParser  = require('body-parser')
-var config      = require('./config.json')
+var bodyParser  = require('body-parser');
+
+var config      = require('./config.json');
+var user        = require('./user.json');
+
 var app         = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
-});
+user.user.push({matrikelNr: "11089451", name: "Mueller", vorname: "Alexander Jan", eMail: "mueller.alexanderjan@icloud.com"});
 
-app.post('/', function(req, res){
-  res.send('POST!');
-});
-
-
-app.listen(config.port);
+for(var i = 0; i < user.user.length; i++)
+{
+    console.log('----------------------')
+    console.log("Matrikel Nr." + user.user[i].matrikelNr);
+}
