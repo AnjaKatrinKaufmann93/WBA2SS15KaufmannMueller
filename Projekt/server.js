4a9c5c97e8d09709e3b8eac579c5f904da3204f3
Anja_Kaufmann <anja@Anjas-MBP.fritz.box>
2015-05-17 20:16:45 +0200

    get, post added
    post funktioniert nicht ?!

--- Projekt/server.js
+++ Projekt/server.js
@@ -3,9 +3,23 @@ var bodyParser = require ('body-parser');
 var users = require ('./user.json');
 var jsonParser = bodyParser.json();
 
+var app = express();
+
+//app.use(express.json()); //support Json-encoded bodies
 
 users.user.push({name:"Kaufmann",vorname:"Anja",matrikelnummer:11096181});
 
 for (var i=0; i<users.user.length;i++){
   console.log(users.user[i].name +" "+users.user[i].vorname + " " + users.user[i].matrikelnummer);
 }
+
+app.get('/', function(req,res){
+  res.json(users);
+});
+
+app.post('/users', jsonParser, function(req,res){
+  users.user.push(req.body);
+  res.type('plain').send('hinzugefügt');
+});
+
+app.listen(3000);
